12b5f30d4aecc57f493c414b67d4d6d7e6629e01
sassela <sassela@users.noreply.github.com>
2020-10-24 16:14:45 +0100

    Step: add drawTile

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,24 @@ module Main where
 import           CodeWorld
 
 picture :: Picture
-picture = storage
+picture = drawTile Wall
 
 data Tile = Wall | Ground | Storage | Box | Blank
+
+-- Exercise
+-- Create a function `drawTile :: Tile -> Picture` that returns a tile according to the following numbers:
+-- 1. wall
+-- 2. ground
+-- 3. storage
+-- 4. box
+-- If the argument is not 1-4, `drawTile` should not draw anything, but should also not crash.
+-- Tip: replace `picture = wall` with `picture = drawTile Wall`. The two statements should be equivalent
+drawTile :: Tile -> Picture
+drawTile Wall    = wall
+drawTile Ground  = ground
+drawTile Storage = storage
+drawTile Box     = box
+drawTile Blank   = blank
 
 wall :: Picture
 wall = coloured brown (solidRectangle 1 1)
